2eba8b71f8bf987db2b60d59fcdb51d7e3e2bdf7
Ankit Kumar Gupta <ankitkumargupta00@gmail.com>
2021-05-16 07:40:58 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { DatePicker, Button, message, Alert } from "antd";
+import "antd/dist/antd.css";
+import { useState } from "react";
 
 function App() {
+  const [date, setDate] = useState("");
+  const handleChange = (value) => {
+    message.info(
+      `Selected Date: ${value ? value.format("YYYY-MM-DD") : "None"}`
+    );
+    setDate(value);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DatePicker onChange={handleChange} />
+      <Button type="primary" style={{ marginLeft: 8 }}>
+        Primary Button
+      </Button>
+      <div style={{ marginTop: 16 }}>
+        Selected Date: {date ? date.format("YYYY-MM-DD") : "None"}
+      </div>
+      <Alert
+        style={{ color: "hotpink" }}
+        message="Selected Date"
+        description={date ? date.format("YYYY-MM-DD") : "None"}
+      />
     </div>
   );
 }
